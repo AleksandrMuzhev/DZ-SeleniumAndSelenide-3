8a8ec93480a5c6822ff4063004fdd4053064d1ad
Aleksand Muzhev <muzhev1990@mail.ru>
2023-06-22 14:53:44 +0500

    Added Tests on Selenide

--- src/test/java/ru/netology/web/DebitCardTest.java
+++ src/test/java/ru/netology/web/DebitCardTest.java
@@ -1,5 +1,7 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
@@ -9,6 +11,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.util.List;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class DebitCardTest {
     private WebDriver driver;
@@ -95,6 +100,69 @@ public class DebitCardTest {
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(new By.ByCssSelector("[data-test-id=phone] span.input__sub")).getText();
         Assertions.assertEquals("Поле обязательно для заполнения", text.trim());
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldSendFormSelenide() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $("form[enctype]");
+        form.$("[data-test-id=name] input").setValue("Александр");
+        form.$("[data-test-id=phone] input").setValue("+79120009999");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        $("[data-test-id=order-success]").shouldHave(Condition.exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldValidateNameFormSelenide() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $("form[enctype]");
+        form.$("[data-test-id=name] input").setValue("Aleksandr");
+        form.$("[data-test-id=phone] input").setValue("+79120009999");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        $("span.input__sub").shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldNoNameFormSelenide() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $("form[enctype]");
+        form.$("[data-test-id=phone] input").setValue("+79120009999");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        $("span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldValidatePhoneFormSelenide() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $("form[enctype]");
+        form.$("[data-test-id=name] input").setValue("Александр");
+        form.$("[data-test-id=phone] input").setValue("+7912000");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        $("[data-test-id=phone] span.input__sub").shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldNoPhoneFormSelenide() throws InterruptedException {
+        open("http://localhost:9999");
+        SelenideElement form = $("form[enctype]");
+        form.$("[data-test-id=name] input").setValue("Александр");
+        form.$(".checkbox__box").click();
+        form.$(".button").click();
+        $("[data-test-id=phone] span.input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
 
         Thread.sleep(5000);
     }
