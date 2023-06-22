7148785f7641346309734c0fce28c2d83eb78d87
Aleksand Muzhev <muzhev1990@mail.ru>
2023-06-22 12:44:21 +0500

    Added Tests on Selenium

--- src/test/java/ru/netology/web/DebitCardTest.java
+++ src/test/java/ru/netology/web/DebitCardTest.java
@@ -42,9 +42,60 @@ public class DebitCardTest {
         driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
         String text = driver.findElement(new By.ByCssSelector("[data-test-id=order-success]")).getText();
-        Assertions.assertEquals("Ваш заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        Assertions.assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
 
         Thread.sleep(5000);
     }
 
+    @Test
+    public void shouldValidateNameForm() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Aleksandr");
+        elements.get(1).sendKeys("+79120009999");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(new By.ByCssSelector("span.input__sub")).getText();
+        Assertions.assertEquals("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.", text.trim());
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldNoNameForm() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(new By.ByCssSelector("[data-test-id=phone] input")).sendKeys("79120009999");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(new By.ByCssSelector("span.input__sub")).getText();
+        Assertions.assertEquals("Поле обязательно для заполнения", text.trim());
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldValidatePhoneForm() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Александр");
+        elements.get(1).sendKeys("+7912000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(new By.ByCssSelector("[data-test-id=phone] span.input__sub")).getText();
+        Assertions.assertEquals("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.", text.trim());
+
+        Thread.sleep(5000);
+    }
+
+    @Test
+    public void shouldNoPhoneForm() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(new By.ByCssSelector("[data-test-id=name] input")).sendKeys("Александр");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(new By.ByCssSelector("[data-test-id=phone] span.input__sub")).getText();
+        Assertions.assertEquals("Поле обязательно для заполнения", text.trim());
+
+        Thread.sleep(5000);
+    }
 }
